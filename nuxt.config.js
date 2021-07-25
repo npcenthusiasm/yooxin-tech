b87847d8075341346e04c18a159b8d3f6044f2bc
npcenthusiasm <npcenthusiasm@gmail.com>
2021-07-25 08:36:17 +0800

    add base seo meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,15 +7,21 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'yooxintech',
+    title: '侑欣科技',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '侑欣科技成立於 2012 年 9 月，初期為 IC 代測加工廠起家，之後再進入記憶體產業從事 FLASHIC、晶片代測、晶片買賣。' },
       { name: 'format-detection', content: 'telephone=no' }
+      // TODO: base seo
+      // { hid: 'og:title', property: 'og:title', content: '' },
+      // { hid: 'og:description', property: 'og:description', content: '' },
+      // { hid: 'og:url', property: 'og:url', content: '' },
+      // { hid: 'og:image', property: 'og:image', content: '' },
+      // { hid: 'og:type', property: 'og:type', content: 'website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
