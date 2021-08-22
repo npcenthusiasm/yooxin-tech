97609c1b9ae89daa4eb2a2058263d0527c41916e
npcenthusiasm <npcenthusiasm@gmail.com>
2021-08-22 11:33:37 +0800

    update quality page

--- pages/quality/index.vue
+++ pages/quality/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div class="container">
-      {{ $route.name }} page
+  <div class="quality-page">
+    <div class="container pt-16 pb-16 md:pt-21 md:pb-21">
+      <picture class="">
+        <source media="(min-width: 768px)" srcset="~assets/img/qualityImg/form_web.png" />
+        <img src="~assets/img/qualityImg/form_mobile.png" alt="品檢留流程圖" />
+      </picture>
+
     </div>
   </div>
 </template>
@@ -9,3 +13,10 @@
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+
+.quality-page {
+
+}
+</style>
